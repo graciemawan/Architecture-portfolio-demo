da36205373fc0b3a9ecd0eb92b5ae748bf2e24c3
graciemawan <graciemawan@gmail.com>
2026-04-07 11:36:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+/*=============== SHOW MENU ===============*/
+
+document.addEventListener('DOMContentLoaded', () => {
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link'),
+    
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on each nav__link, we remove the show menu//
+    navMenu.classList.remove('show-menu')
+}
+    
+navLink.forEach(n => n.addEventListener('click' linkAction))
+    
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+    
+window.addEventListener('scroll', bgHeader) 
+bgHeader()
+/*=============== SWIPER SERVICES ===============*/ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
